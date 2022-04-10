a3a307e25ebde43c0fa794fdf0512465877731a7
Teguh Dana Prayuda <megtrotech@gmail.com>
2022-04-10 12:11:49 +0700

    add App.js

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+
+import { Switch, Route, Link } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+import PostIndex from './pages/posts/Index';
+
+import PostCreate from './pages/posts/Create';
+
+import PostEdit from './pages/posts/Edit';
+
+function App() {
+	return (
+		<div>
+			<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+				<Container>
+					<Navbar.Brand to='/'>
+						Express.JS + React.JS
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+					<Navbar.Collapse id='responsive-navbar-nav'>
+						<Nav className='me-auto'>
+							<Nav.Link
+								as={Link}
+								to='/'
+								className='nav-link'>
+								Home
+							</Nav.Link>
+							<Nav.Link
+								as={Link}
+								to='posts'
+								className='nav-link'>
+								Post
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/posts' component={PostIndex} />
+				<Route
+					exact
+					path='/posts/create'
+					component={PostCreate}
+				/>
+				<Route
+					exact
+					path='/posts/edit/:id'
+					component={PostEdit}
+				/>
+			</Switch>
+		</div>
+	);
+}
+
+export default App;
