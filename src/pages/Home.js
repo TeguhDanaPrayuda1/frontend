88c2e2e9e695289f93a070b191720388bb0e82a6
Teguh Dana Prayuda <megtrotech@gmail.com>
2022-04-10 11:43:39 +0700

    add pages Index.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,33 @@
-import React from "react";
-import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import React from 'react';
+import { Container, Card, Row, Col, Button } from 'react-bootstrap';
 
 function Home() {
-  return (
-    <Container className="mt-3">
-      <Row>
-        <Col md="{12}">
-          <Card className="border-0 rounded shadow-sm">
-            <Card.Body className="p-4">
-              <h1>Express</h1>
-              <p className="lead">
-                Tutorial FullStack Express.js dan React.js oleh{" "}
-                <strong>Teguh Dana Prayuda</strong>
-              </p>
-              <Button href="" variant="primary" size="lg">
-                SELENGKAPNYA
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  );
+	return (
+		<Container className='mt-3'>
+			<Row>
+				<Col md='{12}'>
+					<Card className='border-0 rounded shadow-sm'>
+						<Card.Body className='p-4'>
+							<h1>Express</h1>
+							<p className='lead'>
+								Tutorial FullStack Express.js
+								dan React.js oleh{' '}
+								<strong>
+									Teguh Dana Prayuda
+								</strong>
+							</p>
+							<Button
+								href=''
+								variant='primary'
+								size='lg'>
+								SELENGKAPNYA
+							</Button>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default Home;
